cad6592b1853123e9581e0bfa1c2dd591474e82f
DougAF <31972099+DougAF@users.noreply.github.com>
2019-05-18 19:04:36 -0500

    text and hyperlinks added, scatter display working TODO state abbr in circle and Transitions

--- Code/js/app.js
+++ Code/js/app.js
@@ -1,89 +1,227 @@
 // @TODO: YOUR CODE HERE! http://localhost:8000/
+const svgWidth = 960;
+const svgHeight = 500;
 
-// Load data from csv
-(async function(){
-    const data = await d3.csv("../data/data.csv").catch(error => console.warn(error));
-    console.log(data);
-    // log a list of states
-    const states = data.map(data => data.state);
-    console.log("states", states);
-
-//     // Cast each hours value in tvData as a number using the unary + operator
-//     tvData.forEach(function(data) {
-//         data.hours = +data.hours;
-//         console.log("Name:", data.name);
-//         console.log("Hours:", data.hours);
-//     });
-})()
-// data
-const
-    dataArray = [1, 2, 3],
-    dataCategories = ["one", "two", "three"];
-
-// svg container
-const 
-    svgHeight = 400,
-    svgWidth = 1000;
-
-// margins
 const margin = {
-    top: 50,
-    right: 50,
-    bottom: 50,
-    left: 50
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
 };
 
-// chart area minus margins
-const chartHeight = svgHeight - margin.top - margin.bottom;
-const chartWidth = svgWidth - margin.left - margin.right;
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
 
-// create svg container
-const svg = d3.select("#scatter").append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+const svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
-// shift everything over by the margins
+// Append an SVG group
 const chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// scale y to chart height
-const yScale = d3.scaleLinear()
-  .domain([0, d3.max(dataArray)])
-  .range([chartHeight, 0]);
+// Initial Params
+const chosenXAxis = "age";
 
-// scale x to chart width
-const xScale = d3.scaleBand()
-  .domain(dataCategories)
-  .range([0, chartWidth])
-  .padding(0.05);
+// function used for updating x-scale const upon click on axis label
+function xScale(data, chosenXAxis) {
+  // create scales
+  const xLinearScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
+      d3.max(data, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
 
-// create axes
-const yAxis = d3.axisLeft(yScale);
-const xAxis = d3.axisBottom(xScale);
+  return xLinearScale;
 
-// set x to the bottom of the chart
-chartGroup.append("g")
-  .attr("transform", `translate(0, ${chartHeight})`)
-  .call(xAxis);
+}
 
-// set y to the y axis
-// This syntax allows us to call the axis function
-// and pass in the selector without breaking the chaining
-chartGroup.append("g")
-  .call(yAxis);
+// function used for updating xAxis const upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  const bottomAxis = d3.axisBottom(newXScale);
 
-/* Note: The above code is equivalent to this:
-    const g = chartGroup.append("g");
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
 
-    yAxis(g);
-*/
-// Append Data to chartGroup
-chartGroup.selectAll(".scatter")
-  .data(dataArray)
-  .enter()
-  .append("rect")
-  .classed("bar", true)
-  .attr("x", (d, i) => xScale(dataCategories[i]))
-  .attr("y", d => yScale(d))
-  .attr("width", xScale.bandwidth())
-  .attr("height", d => chartHeight - yScale(d));
+  return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXaxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+    let label  = "";
+    if (chosenXAxis === "age") {
+        label = "Average Age:";
+    }
+    else {
+        label = "Poverty:";
+    }
+
+    const toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.state}<br>${label} ${d[chosenXAxis]}`);
+        });
+
+    circlesGroup.call(toolTip);
+
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
+    })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+        toolTip.hide(data, this);
+    });
+
+  return circlesGroup;
+}
+
+// Retrieve data from the CSV file and execute everything below
+(async function(){
+    const data = await d3.csv("../data/data.csv");
+    console.log(data)
+
+    // parse data
+    data.forEach(function(data) {
+        data.age = +data.age;
+        data.smokes = +data.smokes;
+        data.poverty = +data.poverty;
+    });
+
+    // xLinearScale function above csv import
+    const xLinearScale = xScale(data, chosenXAxis);
+
+    // Create y scale function
+    const yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.smokes)])
+        .range([height, 0]);
+
+    // Create initial axis functions
+    const bottomAxis = d3.axisBottom(xLinearScale);
+    const leftAxis = d3.axisLeft(yLinearScale);
+
+    // append x axis
+    const xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // append y axis
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    // append initial circles
+    let circlesGroup = chartGroup.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d[chosenXAxis]))
+        .attr("cy", d => yLinearScale(d.smokes))
+        .attr("r", 13)
+        .attr("fill", "black")
+        .attr("opacity", ".5");
+    //     .append("text");
+
+    // let text = circlesGroup.selectAll("text")
+    //     .data(data)
+    //     .enter()
+    //     .append("text");
+    //     .attr("x", function(d) { return d.cx; })
+    //     .attr("y", function(d) { return d.cy; })
+    //     .text( function (d) { return "( " + d.cx + ", " + d.cy +" )"; })
+    //     .attr("font-family", "sans-serif")
+    //     .attr("font-size", "20px")
+    //     .attr("fill", "red");
+
+
+    // Create group for  2 x- axis labels
+    const labelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+    const ageLabel = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 20)
+        .attr("value", "age") // value to grab for event listener
+        .classed("active", true)
+        .text("Average citizen age (years)");
+
+    const albumsLabel = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 40)
+        .attr("value", "Poverty") // value to grab for event listener
+        .classed("inactive", true)
+        .text("Poverty (%)");
+
+    // append y axis
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .classed("axis-text", true)
+        .text("Smokers (%)");
+
+    // updateToolTip function above csv import
+    circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+    // x axis labels event listener
+    labelsGroup.selectAll("text")
+        .on("click", function() {
+        // get value of selection
+        const value = d3.select(this).attr("value");
+        if (value !== chosenXAxis) {
+
+            // replaces chosenXAxis with value
+            chosenXAxis = value;
+
+            // console.log(chosenXAxis)
+
+            // functions here found above csv import
+            // updates x scale for new data
+            xLinearScale = xScale(data, chosenXAxis);
+
+            // updates x axis with transition
+            xAxis = renderAxes(xLinearScale, xAxis);
+
+            // updates circles with new x values
+            circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+
+            // updates tooltips with new info
+            circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+            // changes classes to change bold text
+            if (chosenXAxis === "Poverty") {
+                albumsLabel
+                    .classed("active", true)
+                    .classed("inactive", false);
+                ageLabel
+                    .classed("active", false)
+                    .classed("inactive", true);
+            }
+            else {
+                albumsLabel
+                    .classed("active", false)
+                    .classed("inactive", true);
+                ageLabel
+                    .classed("active", true)
+                    .classed("inactive", false);
+            }
+        }
+    });
+})()
